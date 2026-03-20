ac0868e8f9eb8a36a1e16d894cf52ae30fdad565
Sufyan Ali <sufyanali.dev@gmail.com>
2026-03-20 23:09:47 +0500

    Create input for the forms

--- Form_and_Forms-Validation/script.js
+++ Form_and_Forms-Validation/script.js
@@ -1,1 +1,16 @@
-    
+let createAccount = document.querySelector("#create-account");
+
+createAccount.addEventListener(
+    "click",
+    function(detail) {
+
+        detail.preventDefault();
+        let h3 = document.querySelector(".form-title")
+        let loginForHidden = document.querySelector(".login-form")
+
+        h3.style.display = "none"
+        loginForHidden.style.display = "none"; 
+
+        
+    }
+)
