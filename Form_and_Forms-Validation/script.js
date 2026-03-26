b3ca7a8a4136e047503b27d04e7096056a68bede
Sufyan Ali <sufyanali.dev@gmail.com>
2026-03-26 12:19:27 +0500

    Create Account Event Completed

--- Form_and_Forms-Validation/script.js
+++ Form_and_Forms-Validation/script.js
@@ -1,16 +1,25 @@
-let createAccount = document.querySelector("#create-account");
+let formContainer = document.querySelector(".form-container");
+let createNewAccount = document.querySelector("#create-account-button");
 
-createAccount.addEventListener(
+createNewAccount.addEventListener(
     "click",
-    function(detail) {
+    function() {
 
-        detail.preventDefault();
-        let h3 = document.querySelector(".form-title")
-        let loginForHidden = document.querySelector(".login-form")
-
-        h3.style.display = "none"
-        loginForHidden.style.display = "none"; 
-
+        formContainer.innerHTML = `
+        <input type="text" class="input-fields" placeholder="First name" />
+        <input type="text" class="input-fields" placeholder="Last name" />
+        <small>Select date of birth</small>
+        <input type="date" />
+        <small>Select Gender</small>
+        <input type="radio" name="gender-check" value="male" /> Male
+        <input type="radio" name="gender-check" value="female" /> Female
+        <small>Email</small>
+        <input type="email" class="input-fields" placeholder="Email address or phone number" />
+        <small>Password</small>
+        <input type="password" class="input-fields" placeholder="password" />
+        <button type="submit">Login</button>
+        `
+        formContainer.classList.add(".newPage");
         
     }
 )
