ca806510fca48fa3a586f07f41aa0fe751022de0
Sufyan Ali <sufyanali.dev@gmail.com>
2026-07-06 22:34:03 +0500

    Added signup validation with backend

--- Chat_Application_Project/Frontend/JS/authentication.js
+++ Chat_Application_Project/Frontend/JS/authentication.js
@@ -1,25 +1,36 @@
+// Catching HTML Elements
+
 let welcomeScreen = document.querySelector("#welcome-screen");
 let loginScreen = document.querySelector(".login-screen");
 let signupScreen = document.querySelector(".signup-screen");
 let emailScreen = document.querySelector(".email-screen")
-let errorMessage = document.querySelector("#username-or-password-error")
-let passwordError = document.querySelector("#password-error")
+let userNamePasswordLoginError = document.querySelector("#username-error-login")
+let passwordLoginError = document.querySelector("#password-error-login")
+let userNamePasswordSignupError = document.querySelector("#username-error-signup")
+let passwordSignupError = document.querySelector("#password-error-signup")
 let emailError = document.querySelector("#email-error")
 let emailFormatError = document.querySelector("#email-format-error")
 
+// Catching all Buttons 
 const loginButton = document.querySelector("#login-button")
 const signupButton = document.querySelector("#signup-button")
 const nextButton = document.querySelector("#next-button")
-const accountCreateButton = document.querySelector("#account-creation-login-btn")
+const accountSigupButton = document.querySelector("#account-creation-login-btn")
+const accountLoginButton = document.querySelector("#account-login-button")
+const forgotPassword = document.querySelector("#forgot-password")
 
-let inputUserName = document.querySelector("#username-for-signup")
-let inputPassword = document.querySelector("#password-for-signup")
+// Catching all inputs fields
+let inputUserNameForSignup = document.querySelector("#username-for-signup")
+let inputPasswordForSignup = document.querySelector("#password-for-signup")
 let userEmail = document.querySelector("#email")
+let inputUserNameForlogin = document.querySelector("#username-for-login")
+let inputPasswordForlogin = document.querySelector("#password-for-login")
+
+// Login Section all funtionality
 
 loginScreen.classList.add("hide")
 signupScreen.classList.add("hide")
 emailScreen.classList.add("hide")
-// errorMessage.classList.add("error-hide")
 
 loginButton.addEventListener("click", () => {
 
@@ -31,30 +42,60 @@ signupButton.addEventListener("click", () => {
     welcomeScreen.classList.add("hide")
     signupScreen.classList.remove("hide")
 })
+forgotPassword.addEventListener("click", () => {
+
+    loginScreen.classList.add("hide")
+    emailScreen.classList.remove("hide")
+
+})
 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-nextButton.addEventListener("click", () => {
+accountLoginButton.addEventListener("click", () => {    
 
-    // if(!inputUserName.value || !inputPassword.value) {
+    if(!inputUserNameForlogin.value || !inputPasswordForlogin.value) {
         
-    //     passwordError.classList.add("hide")
-    //     errorMessage.classList.remove("hide")
-    // }
-    // else if (!passwordRegex.test(inputPassword.value)) {
+        passwordLoginError.classList.add("hide")
+        userNamePasswordLoginError.classList.remove("hide")
+    }
+    else if (!passwordRegex.test(inputPasswordForlogin.value)) {
 
-    //     errorMessage.classList.add("hide")
-    //     passwordError.classList.remove("hide")
-    // }
-    // else {
+        userNamePasswordLoginError.classList.add("hide")
+        passwordLoginError.classList.remove("hide")
+    }
+    else {
+
+        console.log("True all codidtions");
+        
+    }
+})
+
+
+// Signup Section all functionality
+
+nextButton.addEventListener("click", () => {
+    
+
+    if(!inputUserNameForSignup.value || !inputPasswordForSignup.value) {
+        
+        passwordSignupError.classList.add("hide")
+        userNamePasswordSignupError.classList.remove("hide")  
+        
+    }
+    else if (!passwordRegex.test(inputPasswordForSignup.value)) {
+
+        userNamePasswordSignupError.classList.add("hide")
+        passwordSignupError.classList.remove("hide")
+    }
+    else {
 
         signupScreen.classList.add("hide")
         emailScreen.classList.remove("hide")
-    // }
+    }
 })
 
-accountCreateButton.addEventListener("click", () => {
+accountSigupButton.addEventListener("click", async () => {
 
     if (!userEmail.value) {
         
@@ -65,5 +106,34 @@ accountCreateButton.addEventListener("click", () => {
         
         emailError.classList.add("hide")
         emailFormatError.classList.remove("hide")
+    }
+    else {
+        emailFormatError.classList.add("hide")
+              try {
+            const response = await fetch("http://localhost:5000/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    username: inputUserNameForSignup.value,
+                    email: userEmail.value,
+                    password: inputPasswordForSignup.value
+                })
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                alert(data.message);
+                window.location.href = "chat.html";
+            } else {
+                alert(data.message);
+            }
+
+        } catch (error) {
+            console.log("Error:", error);
+            alert("Something went wrong. Please try again.");
+        }
     }
 })
